333040667d89ee826a54007ba78d54fd99801511
Prasen Biswas <prasenbiswas25@gmail.com>
2023-12-27 16:37:43 +0530

    Initial Commit

---
+++ CRUD/CRUD_Web/Controllers/CategoryController.cs
@@ -0,0 +1,114 @@
+﻿using CRUD_Web.Data;
+using CRUD_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _context.categories.ToList();
+            if(objCategoryList == null)
+            {
+                return NotFound();
+            }
+            return View(objCategoryList);
+        }
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.categories.Add(obj);
+                _context.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _context.categories.Find(id);
+            //var categoryFirst = _context.categories.FirstOrDefault(c => c.Id == id);
+            //var categorySecond = _context.categories.SingleOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.categories.Update(obj);
+                _context.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _context.categories.Find(id);
+            //var categoryFirst = _context.categories.FirstOrDefault(c => c.Id == id);
+            //var categorySecond = _context.categories.SingleOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //Post
+        //[HttpPost,ActionName("Delete")] That is also work when we do post action .
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _context.categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _context.categories.Remove(obj);
+            _context.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUD/CRUD_Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD_Web.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CRUD_Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> categories { get; set; }
+    }
+}
